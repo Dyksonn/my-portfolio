f36672952a29f3ad6318895de3f15a8b0eda10f1
Dykson <44515719+Dyksonn@users.noreply.github.com>
2025-01-16 19:46:38 -0300

    initialize project with essential files and Sentry integration

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "Sobre", link: "#about" },
+  { name: "Projetos", link: "#projects" },
+  { name: "Experiências", link: "#experience" },
+  { name: "Contato", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Priorizo ​​a colaboração com o cliente, promovendo uma comunicação aberta",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Sou muito flexível com comunicações de fuso horário",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tecnologias",
+    description: "Eu tento melhorar constantemente",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Entusiasta de tecnologia apaixonado por desenvolvimento.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Atualmente construindo meu proprio aplicativo de carro",
+    description: "Em construção ...",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Você quer começar um projeto junto a mim?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Seu ponto de coleta de resíduos é aqui!",
+    des: "A ideia principal é ajudar as pessoas a encontrar pontos de coleta de lixo de forma eficiente.",
+    img: "/pd-1.jpeg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/Dyksonn/NLW---Ecoleta-?tab=readme-ov-file",
+  },
+  {
+    id: 2,
+    title: "Viaje de trem pela Europa com suas milhas",
+    des: "Conheça paisagens únicas, diferentes culturas e crie momentos inesquecíveis.",
+    img: "/pd2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://www.smiles.com.br/mfe/trens",
+  },
+  {
+    id: 3,
+    title: "SMARTLOAD APP",
+    des: "A lógica na sequência do App trará o controle operacional, dando visibilidade da localização via azimute, além de contar com serviços de mensageria possibilitando a comunicação do evento de sinistro pelo próprio motorista.",
+    img: "/pd3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://play.google.com/store/apps/details?id=br.com.smartload.app&hl=en&pli=1",
+  },
+  {
+    id: 4,
+    title: "App de plantas animado com React Native",
+    des: "Consulta pela camera e informações sobre a planta.",
+    img: "/pd5.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/Dyksonn/PlantManager",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Dykson was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dykson's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dykson is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Dykson was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dykson's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dykson is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Dykson was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dykson's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dykson is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Dykson was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dykson's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dykson is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Dykson was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Dykson's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Dykson is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Developer Full Stack - Softvaro",
+    desc: "Ajudei no desenvolvimento de uma plataforma nova para Smiles, utilizando ReactJs Micro Frontends, Aumentando a interatividade.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Developer Mobile Sr - TradeUp",
+    desc: "Projetei e desenvolvi aplicativos móveis para iOS e Android, com foco em usabilidade e desempenho para empresa TIM. Ajudando clientes a terem uma melhor experiência com a marca.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Developer Mobile Sr - SmartLoad",
+    desc: "Desenvolvi e arquitetei o aplicativo SmartLoad, um aplicativo de rastreamento de carga para motoristas de caminhão.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Developer Mobile Pleno II - Smit",
+    desc: "Desenvolvi diversos aplicativos nessa startup de tecnologia, Aplicativos de delivery, aplicativos de rastreamento de carga e aplicativos de controle de estoque.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Dyksonn",
+  },
+  {
+    id: 2,
+    img: "/wha.svg",
+    link: "https://wa.me/5511960644064",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/dykson-santos-410740187/",
+  },
+];
