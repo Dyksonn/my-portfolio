e4e167e47e3871b549caff7acd0f257825c73ade
Dykson <44515719+Dyksonn@users.noreply.github.com>
2025-01-20 08:41:13 -0300

    Ajuste para subir para prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000, // 25 segundos
+    pagesBufferLength: 2,
+  },
+  experimental: {
+    runtime: 'nodejs',
+    serverComponentsExternalPackages: ['react', 'react-dom'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/page',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
